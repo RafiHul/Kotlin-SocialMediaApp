42d5edb7e0a637d59defd5d69272aad5b0fcfc6c
Rafihul <rafihul2905@gmail.com>
2024-11-12 08:55:11 +0700

    setup: new post field

---
+++ app/src/main/java/com/rafih/socialmediaapp/fragment/NewUserPostFragment.kt
@@ -0,0 +1,36 @@
+package com.rafih.socialmediaapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import com.rafih.socialmediaapp.R
+import com.rafih.socialmediaapp.databinding.FragmentNewUserPostBinding
+
+class NewUserPostFragment : Fragment() {
+
+    private var _binding: FragmentNewUserPostBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentNewUserPostBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
